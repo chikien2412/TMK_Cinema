0697d6ebb78d6f9bb030a027d22deea1cf555d62
Cao Van Tan <minhtan006237@gmail.com>
2021-12-25 22:29:17 +0700

    up form

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once '../partials_front/header.php';
+?>
+
+<h1>HELLO</h1>
+
+<?php
+include_once '../partials_front/footer.php';
+?>
---
+++ view/customer/home.php
@@ -0,0 +1,10 @@
+<?php
+include_once '../../partials_front/header.php';
+include_once '../../partials_front/header_customer.php';
+?>
+
+<h1>HELLO</h1>
+
+<?php
+include_once '../../partials_front/footer.php';
+?>
