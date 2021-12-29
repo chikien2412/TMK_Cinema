51d8e563b886525d9afaf214e22b0d72cf2b1f6f
NguyenManh <manh95405@gmail.com>
2021-12-29 21:57:44 +0700

    Add file

---
+++ view/staff/staff_inf_phim.php
@@ -0,0 +1,55 @@
+<?php
+include_once '../../partials_front/header.php';
+?>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 staff_col">
+                <h3 style='text-align: center ;  text-transform:uppercase ;'>Thông tin phim</h3>
+            </div>
+            <div class="col-12">
+                <form action="../staff/staff_add_phim.php" method="get">
+                <button type="submit" class="btn btn-primary staff-add"><i class="fas fa-plus-circle"></i> Thêm phim</button>
+                </form>
+            </div>
+            
+            <div class="col-12">
+                <table class="table table-striped staff-table">
+                    <thead>
+                        <tr class="table-success">
+                            <th scope="col">Tên phim</th>
+                            <th scope="col">Mô tả</th>
+                            <th scope="col">Thể Loại</th>
+                            <th scope="col">Thời lượng</th>
+                            <th scope="col">Ngày KC</th>
+                            <th scope="col">Sản xuất</th>
+                            <th scope="col">Đạo diễn</th>
+                            <th scope="col">Diễn viên</th>
+                            <th scope="col">Năm SX</th>
+                            <th scope="col">Sửa</th>
+                            <th scope="col">Xóa</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                            <td>Chiến thần vô cực</td>
+                            <td>abc</td>
+                            <td>Hoạt hình</td>
+                            <td>150 phút</td>
+                            <td>30/06/2021</td>
+                            <td>Mỹ</td>
+                            <td>adshd</td>
+                            <td>ácc</td>
+                            <td class= 'staff-row' >2021</td>
+                            <td><a href="../staff/staff_update_phim.php"><i class="fas fa-edit"></i></a></td>
+                            <td><a href="#"><i class="fas fa-trash-alt"></i></a></td>
+                        </tr>
+                        
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</main>
---
+++ view/staff/staff_update_phim.php
@@ -0,0 +1,103 @@
+<?php
+include_once '../../partials_front/header.php'
+?>
+
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 staff_col">
+                <h3 style='text-align: center ;  text-transform:uppercase ; margin-bottom: 30px; margin-top: 30px;'>Sửa
+                    thông tin</h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="staticTenPhim" class="col-3 col-form-label">Tên phim: </label>
+                    <div class="col-9">
+                        <input type="text" class="form-control" style="height: 35px; width: 100%;" id="inputTenPhim">
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="staticSX" class="col-3 col-form-label">Sản xuất: </label>
+                    <div class="col-9">
+                        <input type="text" class="form-control" style="height: 35px; width: 100%;" id="inputSX">
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="inputMoTa" class="col-3 col-form-label">Mô tả:</label>
+                    <div class="col-9">
+                        <input type="text" class="form-control" style="height: 35px; width: 100%;" id="inputMoTa">
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="inputDD" class="col-3 col-form-label">Đạo diễn:</label>
+                    <div class="col-9">
+                        <input type="text" class="form-control" style="height: 35px; width: 100%;" id="inputDD">
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="inputTheLoai" class="col-3 col-form-label">Thể loại:</label>
+                    <div class="col-9">
+                        <input type="text" class="form-control" style="height: 35px; width: 100%;" id="inputTheLoai">
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="inputDV" class="col-3 col-form-label">Diễn viên:</label>
+                    <div class="col-9">
+                        <input type="text" class="form-control" style="height: 35px; width: 100%;" id="inputDV">
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-4 row">
+                    <label for="inputTL" class="col-sm-3 col-form-label">Thời lượng:</label>
+                    <div class="col-9">
+                        <input type="text" class="form-control" style="height: 35px; width: 100%;" id="inputTL">
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="inputDD" class="col-3 col-form-label">Năm SX:</label>
+                    <div class="col-9">
+                        <select name="example" class="staff_ComB">
+                            <option value="A">2019</option>
+                            <option value="B">2020</option>
+                            <option value="-">2021</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 ms-5 mt-3 me-4">
+                <div class="mb-3 row">
+                    <label for="inputTL" class="col-3 col-form-label">Ngày KC:</label>
+                    <div class="col-sm-9">
+                        <input type="date" name="" class="staff_date" style="" id="">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-10" style="margin-left: 300px;margin-top: 20px;">
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                        <button class="btn btn-secondary me-md-2" style="width: 140px; margin: 8px" type="button">Xác
+                            nhận</button>
+                        <form action="../staff/staff_inf_phim.php" method="get">
+                            <button class="btn btn-secondary" style="width: 140px; margin: 8px" type="submit">Quay
+                                lại</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+</main>
