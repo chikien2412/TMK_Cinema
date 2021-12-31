0c3617fa2627c38797c69811d76ffceb6899a55f
NguyenManh <manh95405@gmail.com>
2021-12-31 21:58:54 +0700

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,4 +60,22 @@ $(document).ready(function() {
             modal.hide();
         }
     });
+    $(".btn_cancel").click(function() {
+        $(".Cancel").show();
+    });
+    $(".js_No").click(function() {
+        $(".Cancel").hide();
+    })
+    $(".js_addinf").click(function() {
+        $(".js_add").show();
+    });
+    $(".js_OK").click(function() {
+        $(".js_add").hide();
+    })
+    $(".js_updateinf").click(function() {
+        $(".js_update").show();
+    });
+    $(".js_ok").click(function() {
+        $(".js_update").hide();
+    })
 });
