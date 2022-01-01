f843acf1594844aac699525196e527ff56703d3f
chikien2412 <chikien2412@gmail.com>
2022-01-01 10:59:14 +0700

    update

--- view/staff/home.php
+++ view/staff/home.php
@@ -43,16 +43,16 @@ include_once '../../partials_front/header_nav_staff.php';
                                 </tr>
                             </thead>
 
-                            <tbody>
-                                <tr>
+                            <tbody >
+                                <tr >
                                     <td>Lương Chí Kiên</td>
                                     <td>24/12/2001</td>
                                     <td>0395123123</td>
                                     <td>lck@gmail.com</td>
                                     <td>Thái Nguyên</td>
                                     <td>Nam</td>
-                                    <td><a href="edit.php"><i class="fas fa-user-edit "></i></a></td>
-                                    <td><a href="delete.php"><i class="fas fa-user-slash"></i></a> </td>
+                                    <td><a href="edit.php"class="ms-3"><i class="fas fa-edit"></i></a></td>
+                                    <td><button type="submit" class = "btn_cancel staff_Cancle ms-3" style = "border: none;"> <i class="fas fa-trash-alt"></i></button></td>
 
                                 </tr>
                             </tbody>
@@ -66,6 +66,18 @@ $(document).ready(function() {
     $('#example').DataTable();
 });
 </script>
+<section class = "Cancel" style = "position: fixed;top: 0;left: 0;right: 0;bottom: 0;display: none">
+    <div class="cancel-container" style = "background-color: #fff;height: 145px;margin-left: 40%;margin-right: 40%;padding-bottom: 100px;border-radius: 10px;margin-top: 20%;">
+        <div class="row">
+            <div class="col-12" style = "text-align: center;font-size: 16px;margin-top: 30px;margin-left: 5px;">
+            <h5>Bạn có muốn xóa không?</h5>
+            </div>
+            <button type="submit" class="btn btn-primary staff-add" style = "margin-left: 125px;width: 50px;">Yes</button>
+            <button type="submit" class=" js_No btn btn-primary staff-add" style = "margin-left: 0px;width: 50px;">No</button>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 include_once '../../partials_front/footer.php';
 ?>
