f5357cd8222c5ecebf7c59bf2c1a118f26adb540
Cao Van Tan <minhtan006237@gmail.com>
2022-01-03 20:21:55 +0700

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,15 +48,6 @@ $(document).ready(function() {
         }
     });
 
-    // Process Login
-    $("#btnLogin").click(function() {
-        var userName = $("#inputUserName").val();
-        var password = $("#inputPassword").val();
-
-        if (userName == "" || password == "") {
-            alert("Bạn phải nhập tài khoản và mật khẩu!");
-        }
-    });
     $(".js_No").click(function() {
         $(".Cancel").hide();
     })
@@ -81,12 +72,72 @@ $(document).ready(function() {
     $(".Icon_time").click(function() {
         $(".js_update").hide();
     })
+
+    // Process Login
+    $("#btnLogin").click(function() {
+        var userName = $("#inputUserName").val();
+        var password = $("#inputPassword").val();
+
+        if (userName == "" || password == "") {
+            alert("Bạn phải nhập tài khoản và mật khẩu!");
+        }
+    });
 });
 
 
-// var myModal = document.getElementById('myModal')
-// var myInput = document.getElementById('myInput')
+// Login
+let signin_form = document.querySelector('#signin-form')
 
-// myModal.addEventListener('shown.bs.modal', function () {
-//   myInput.focus()
-// })
+let signin_btn = document.querySelector('#signin-btn')
+
+validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    return re.test(String(email).toLowerCase())
+}
+
+checkSigninInput = (input) => {
+    let err_span = signin_form.querySelector(`span[data-err-for="${input.id}"]`)
+    let val = input.value.trim()
+    let form_group = input.parentElement
+
+    switch(input.getAttribute('type')) {
+        case 'password':
+            if (val.length < 6) {
+                form_group.classList.add('err')
+                form_group.classList.remove('success')
+                err_span.innerHTML = 'Password must be at least 6 characters'
+            } else {
+                form_group.classList.add('success')
+                form_group.classList.remove('err')
+                err_span.innerHTML = ''
+            }
+            break;
+        case 'email':
+            if (val.length === 0 || !validateEmail(val)) {
+                form_group.classList.add('err')
+                form_group.classList.remove('success')
+                err_span.innerHTML = 'Email is invalid'
+            } else {
+                form_group.classList.add('success')
+                form_group.classList.remove('err')
+                err_span.innerHTML = ''
+            }
+    }
+}
+
+checkSigninForm = () => {
+    let inputs = signin_form.querySelectorAll('.form-input')
+    inputs.forEach(input => checkSigninInput(input))
+}
+
+signin_btn.onclick = () => {
+    checkSigninForm()
+}
+
+let inputs = signin_form.querySelectorAll('.form-input')
+inputs.forEach(input => {
+    input.addEventListener('focusout', () => {
+        checkSigninInput(input)
+    })
+})
